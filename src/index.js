b274a656851594704be9cb806e3211f6e48876fb
gabrielo <espinola.gabriel@gmail.com>
2018-06-19 17:00:31 +0000

    commiting new project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const API_KEY = 'AIzaSyAj6b2wsW_a6jC49vWePSU5DaJW2T7bx6I';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();

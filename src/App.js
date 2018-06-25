24bb18c0c8854209e903f57e136fbeb0460dbb71
gabrielo <espinola.gabriel@gmail.com>
2018-06-25 21:01:00 +0000

    changing first search term

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
             selectedVideo: null
             };
         
-        this.videoSearch('surfboards');
+        this.videoSearch('doge');
         
     }
     
